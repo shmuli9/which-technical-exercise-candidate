5e80ab01b36223b332b1ab028ab83de80e24cd20
Shmuli Margulies <shmuli9@gmail.com>
2024-08-01 16:04:41 +0100

    add input/output types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
 import fs from 'fs';
 
+enum Heading {
+  north = 'north',
+  east = 'east',
+  south = 'south',
+  west = 'west',
+}
+type HeadingType = keyof typeof Heading;
+
+enum Command {
+  forward = 'forward',
+  left = 'left',
+  right = 'right',
+}
+type CommandType = keyof typeof Command;
+
+enum Status {
+  ok = 'ok',
+  error = 'error',
+  crash = 'crash',
+}
+type StatusType = keyof typeof Status;
+
+type Coordinate = { x: number; y: number };
+
+interface RobotState {
+  location: Coordinate;
+  heading: HeadingType;
+}
+
+interface RobotInput extends RobotState {
+  arena: { corner1: Coordinate; corner2: Coordinate };
+  directions: CommandType[];
+}
+
+interface RobotOutput extends RobotState {
+  status: StatusType;
+  path: CommandType[];
+}
+
 export function runWith(_input: any) {
   return { status: 'error' };
 }
